52e14af32f3f650ea45702abb158c88219dad95d
Ruan vitor <72428158+Ruan-codeVi@users.noreply.github.com>
2021-04-01 18:40:35 -0300

    up

---
+++ Estrutura de Dados- Conjuntos/conjuntos.js
@@ -0,0 +1,11 @@
+function conjunto(){
+    let itens = {}
+
+ this.add = function(valor){
+     // Adicionar um novo item ao conjunto
+ }
+
+ this.remover = function(valor){
+    //  remove um item ao conjunto
+ }
+}
